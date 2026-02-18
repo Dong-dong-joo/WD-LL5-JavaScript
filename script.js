c4ab034fe24568c4cb0b9380efca8132b2296a34
Donghyeon Kim <dkim234@gmu.edu>
2026-02-18 00:03:15 +0000

    add name

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 console.log("Hello, World!");
 console.error("This is my first JavaScript error!");
+alert("Donghyeon Kim");
+alert("Joshua Garnett");
+alert("Jacob Roginski");
+alert("Jonathan Luna");
+alert("Nidhi Lalwani");
